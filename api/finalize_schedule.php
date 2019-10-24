5e23661976e2913dd632bfca120249b67e6c7cfc
crossa95 <43677395+crossa95@users.noreply.github.com>
2019-10-24 12:14:04 -0400

    Add files via upload
    Latest Fairness Algo, SQL dump

--- api/finalize_schedule.php
+++ api/finalize_schedule.php
@@ -1,17 +1,57 @@
+  
 <?php
 #finalizes the schedule by finding the min points,
 #then moving that user, desktop, and timeslot info
 #to the reservation table.
+#This file then emails users who received reservations,
+#Resets users with negative point values to zero points
+#And then subtracts points from users who have not made a recent request
 #Author: Alex Cross
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'Exception.php';
+require 'PHPMailer.php'; # PHPMailer files to send emails via an SMTP
+require 'SMTP.php';
+
+# Default values for fairness algorithm
+$primeMod = 5;
+$nonPrimeMod = -1;
+$consolation = -2;
+$daysIdle = 7;
+$idleDeduction = -3;
+
+# Load custom configuration if available
+
+if($xml=simplexml_load_file("../configuration.xml")){
+    $primeMod = $xml->primemod;
+    $nonPrimeMod = $xml->nonprime;
+    $consolation = $xml->consolation;
+    $daysIdle = $xml->days_to_idle;
+    $idleDeduction = $xml->idle_user;
+
+    #Did not have SMTP information to hard code; Would advise against
+    $smtp_mode = $xml->stmp_mode;
+    $smtp_port = $xml->stmp_port;
+    $smtp_username = $xml->smtp_username;
+    $stmp_password= $xml->stmp_password;
+    $stmp_host= $xml->stmp_host;
+    $stmp_type= $xml->stmp_type;
+    $stmp_outgoing_name= $xml->stmp_outgoing_name;
+    $stmp_subject= $xml->stmp_subject;
+    $stmp_content= $xml->stmp_content;
+}
+
+
 header("Content-Type: application/json");
-if(!include('../connect.php')){
+if(!include('../connect.php'))
+{
     die('error retrieving connect.php');
 }
+
 $dbh = ConnectDB();
 $dbh2 = ConnectDB();
-try {
-  // $slot_id = $_GET['slot_id'];
-  // $dtop_id = $_GET['dtop_id'];
+
   $sql = 'CALL getQueue()';
   
   $stmt = $dbh->prepare($sql);
@@ -22,7 +62,8 @@ try {
   
   
   
-  foreach($stmt->fetchAll() as $row){
+  foreach($stmt->fetchAll() as $row)
+  {
             //vars that go into the SQL input
             $desktop = $row['dtop_id'];
             $slot =  $row['slot_id'];
@@ -40,25 +81,67 @@ try {
             $result = $stmt2->fetch();
             $minpoints = $result['points'];
             $desktop = $result['dtop_id'];
-            $slot =  $result['slot_id']; // Lines 43-44 probably unnecessary
+            $slot =  $result['slot_id'];
+
             //SQL Procedure: Copies entry from Queue -> Reservation, then removes the entry from Queue
-            $move = "CALL moveToReservation(".$minpoints.",".$desktop.",".$slot.");";
+            $move = "CALL mtrTest(".$minpoints.",".$desktop.",".$slot.",". $primeMod . ",
+                ".$nonPrimeMod.",".$consolation.");";
+
             $stmt2 = $dbh2->prepare($move);
-            $stmt2->execute();
-            //TODO: Add points for primetime slots, subtract points for non-primetime slots,
-            //do something with the remaining queue members
-            
-           /*  echo $desktop ?? 'DESKTOP NOT FOUND'; //testing purposes - if value is null, outputs Not found
-            echo ' ';          
-            echo $slot ?? 'SLOT NOT FOUND';
-            echo ' ';
-            echo $minpoints ?? 'POINTS NOT FOUND';
-            echo ' '; */
-             
-            
-  }
-  
-} catch(Exception $e){
-    die($e);
+            $stmt2->execute();          
 }
+
+  # Find unique e-mails for blast
+  $email = 'select distinct email from user join reservation using (user_num)';
+
+  $stmt2 = $dbh2->prepare($email);
+  $stmt2->execute();
+
+if($xml=simplexml_load_file("../configuration.xml")){
+   foreach($stmt2->fetchAll() as $row)
+  {
+    $email = $row['email'];
+    $mail = new PHPMailer();
+    $mail->IsSMTP();
+    $mail->SMTPDebug = 0;
+    $mail->SMTPAuth = TRUE;
+    $mail->SMTPSecure = "tls";
+    $mail->Port     = 587;  
+    $mail->Username = "asrcscheduler@gmail.com";
+    $mail->Password = "!QAZ1qazlizard";
+    $mail->Host     = "smtp.gmail.com";
+    $mail->Mailer   = "smtp";
+    $mail->SetFrom("asrcscheduler@gmail.com", "Schedule Genie");
+    $mail->AddReplyTo("No-reply", "PHPPot");
+    $mail->AddAddress($email);
+    $mail->Subject = "New Schedule Has Been Finalized (Do Not Reply)";
+    $mail->WordWrap   = 80;
+    $content = "<b>This e-mail is a notification that you have been selected for testing time slot(s) for the upcoming week.</b>"; 
+    $mail->MsgHTML($content);
+    $mail->IsHTML(true);
+        if(!$mail->Send()) 
+            echo "Problem sending email.";
+        else 
+        echo "email sent.\n";
+    }
+} 
+    // Sets users with < 0 points to 0
+    $move = "CALL fix_points();";
+    $stmt2 = $dbh2->prepare($move);
+    if(!$stmt2->execute())
+        echo "Failed to fix points\n";
+    
+    // Subtracts points from users without a recent request.
+    $move = "CALL check_last_requests($daysIdle, $idleDeduction)";
+    $stmt3 = $dbh2->prepare($move);
+    if(!$stmt3->execute())
+        echo "Failed to check_last_requests\n";
+
+    # Resets all user requests to 0.
+    $move = "CALL reset_requests()";
+    $stmt3 = $dbh2->prepare($move);
+    if(!$stmt3->execute())
+        echo "Failed to reset request column\n";
+ 
+
 ?>
--- joinQueue.php
+++ joinQueue.php
@@ -3,16 +3,19 @@
 
 #inserts a row into the queue table with the user_num, slot_id, dtop_id, and the current wait position in the queue
 #author: Cassandra Bailey
+#modified: Alex Cross
 
         if  (!include('connect.php')) {
                 die('error finding connect file');
         }
 
         $dbh = ConnectDB();
-?>
+/* ?>
 
-<?php
+<?php */
 
+
+	$actualWait = 0;
 	$curr = $_POST['curr'];
 	$date = $_POST['date'];
 	$time = $_POST['time'];
@@ -41,53 +44,22 @@
 	$stmt = null;
 
 	if($currPos[0] == null){
-
-
-		#gets the largest wait position value for the desktop in the timeslot provided
-
-		$sql  = "SELECT MAX(wait_position) FROM queue WHERE slot_id = '$slot[0]' AND dtop_id = '$desktop'";
-		$stmt = $dbh->prepare($sql);
-		$stmt->execute();
-		$maxWait = $stmt->fetch();
-		$stmt = null;
-		
-
-		#if the query returns null, then no one is in the queue for the desktop in the timeslot provided
-		#the first wait position value is automatically set to 0
-	
-		if($maxWait[0] == null){
-			$actualWait = 0;
-		}
-
-
-		#if the query does not return null, then one or more users are already in the queue for the desktop in the 
-		#timeslot provided; the current user will be inserted at the back of the queue with a wait position 1 greater
-		#than the maximum wait position
-
-		else{
-			$actualWait = $maxWait[0] + 1;
-		}
-
-
-		#checks to see how many people are waiting in the queue for the desktop at the timeslot provided
-		#used in the echo statement 
-
-		$sql  = "SELECT COUNT(*) FROM queue WHERE slot_id = '$slot[0]' AND dtop_id = '$desktop'";
-                $stmt = $dbh->prepare($sql);
-                $stmt->execute();
-                $numInLine = $stmt->fetch();
-                $stmt = null;
 		
 		
 		#inserts the reservation in the queue				
 
-                $sql  = "INSERT INTO queue (dtop_id, slot_id, user_num, wait_position) ";
-                $sql .= "VALUES ('$desktop', '$slot[0]', '$curr', '$actualWait')";
+                $sql  = "INSERT INTO queue (dtop_id, slot_id, wait_position, user_num,request_time) ";
+                $sql .= "VALUES ('$desktop', '$slot[0]', '$actualWait', '$curr',(select now()))";
                 $stmt = $dbh->prepare($sql);
-                $stmt->execute();
+				$stmt->execute();
+				
+				$sql = "CALL set_last_request($curr);";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
 
 
                 $row  = "You have joined the queue for desktop " . $desktop . " at " . $time . " on " . $date;
+                //$row .= ". Number of people in front of you: " . $numInLine;
                 echo $row;
 	}
 	else{
