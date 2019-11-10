d5baa944acd3855a742c316b4c29cb2d13fd7875
anchet19 <49288517+anchet19@users.noreply.github.com>
2019-11-10 12:30:27 -0500

    Added admin password reset, cleaned up duplicate code issues

--- api/admin_change_password.php
+++ api/admin_change_password.php
@@ -9,6 +9,26 @@ if(!include_once("../Utils/connect.php")){
   die("error finding connect file");
 }
 
-if(isset($_POST[""]))
+try {
+  $db = ConnectDB();
+  $user = $_POST["user-select"];
+  $new_pass = $_POST["newPassword"];
+  $new_pass_c = $_POST["newPassword_confirm"];
 
+  if(empty($user) || empty($new_pass) || empty($new_pass_c)) {
+    echo json_encode('{"result": false, "message": "All fields required."}');
+  }
+  else if($new_pass == $new_pass_c) {
+    $new_pass = password_hash($new_pass,PASSWORD_DEFAULT);
+    $sql = "UPDATE user SET password='$new_pass', login_attempts = 0 WHERE user_num='$user'";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    echo json_encode(array("result" => true, "message" => "Password updated successfully."));
+  } 
+  else {
+    echo json_encode(array("result" => false, "message" => "Passwords do not match."));
+  }
+} catch (\Error $e) {
+  echo json_encode($e->getMessage());
+}
 ?>
