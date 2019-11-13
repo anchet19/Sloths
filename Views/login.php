774f5982403e66e8c8d04028aa987379462b44c6
anchet19 <49288517+anchet19@users.noreply.github.com>
2019-11-13 09:16:32 -0500

    removal of docCookies final

--- Views/login.php
+++ Views/login.php
@@ -19,8 +19,7 @@ modified: Chris Ancheta, Kyle Kaminski -->
 
     <!-- CSS specifically for this page. -->
     <link rel="stylesheet" href="../Styles/login.css" />
-
-    <script src="../Utils/docCookies.js"></script>
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </head>
   <body class="background" background="../Images/Background.png">
