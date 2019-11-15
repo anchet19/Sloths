6eb82797ca0539fa4a3dad8d15b795753c05f898
crossa95 <43677395+crossa95@users.noreply.github.com>
2019-11-15 18:22:00 -0500

    three states

--- api/joinQueue.php
+++ api/joinQueue.php
@@ -15,7 +15,7 @@
 	$desktop = $_POST['desktop'];
 	
 	#gets the slot_id for the date and start_time provided
-	$sql  = "SELECT slot_id FROM timeslot ";
+	$sql  = "SELECT slot_id, state FROM timeslot ";
 	$sql .= "WHERE date = '$date' AND start_time = '$time'";
 	$stmt = $dbh->prepare($sql);
 	$stmt->execute();
@@ -30,24 +30,74 @@
 	$stmt = $dbh->prepare($sql);
 	$stmt->execute();
 	$currPos = $stmt->fetch();
-  $stmt = null;
+	$stmt = null;
+	
+	
   
 	if($currPos[0] == 0){
+		if($slot[1] == 0){ #First state check
 		#inserts the request into the queue				
-    $sql  = "INSERT INTO queue (dtop_id, b_num, slot_id, wait_position, user_num, request_time) ";
-    $sql .= "VALUES ('$desktop', '$build', '$slot[0]', '$actualWait', '$curr',(select now()))";
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
+    	$sql  = "INSERT INTO queue (dtop_id, b_num, slot_id, wait_position, user_num, request_time) ";
+    	$sql .= "VALUES ('$desktop', '$build', '$slot[0]', '$actualWait', '$curr',(select now()))";
+    	$stmt = $dbh->prepare($sql);
+    	$stmt->execute();
     
-    $sql = "CALL set_last_request($curr);";
-    $stmt = $dbh->prepare($sql);
-    $stmt->execute();
-    $row  = "You have joined the queue for desktop " . $desktop . " at " . $time . " on " . $date;
+    	$sql = "CALL set_last_request($curr);";
+    	$stmt = $dbh->prepare($sql);
+    	$stmt->execute();
+    	$row  = "You have joined the queue for desktop " . $desktop . " at " . $time . " on " . $date;
 
-    echo $row;
-	}
-	else{
-		#echo "You are already in the queue for desktop " . $desktop . " at " . $time . " on " . $date;
-		echo "A request for this timeslot already exists for this user.";
-	}
+    	echo $row;
+		}
+
+		if($slot[1] == 1){ #Second State Check -- 2nd round pick
+			$sql ="SELECT count(*) from leftover where user_num = '$curr' ";
+			$stmt = $dbh->prepare($sql);
+			$stmt->execute();
+			$eligible = $stmt->fetch();
+			if($eligible[0] > 0){
+				$sql  = "INSERT INTO queue (dtop_id, b_num, slot_id, wait_position, user_num, request_time) ";
+				$sql .= "VALUES ('$desktop', '$build', '$slot[0]', '$actualWait', '$curr',(select now()))";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+			
+				$sql = "CALL set_last_request($curr);";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$row  = "You have joined the queue for desktop " . $desktop . " at " . $time . " on " . $date;
+
+				echo $row;
+			}else{
+				echo "This user is ineligible for second round picks.";
+			}
+		}
+		if($slot[1] == 2){ #3rd state check -- FCFS FFA
+			$sql  = "SELECT count(*) FROM reservation WHERE slot_id = '$slot[0]' AND (dtop_id = '$desktop' OR user_num = '$curr') "; #crossmod		
+			$stmt = $dbh->prepare($sql);
+			$stmt->execute();
+			$eligible = $stmt->fetch();
+				if($eligible[0] == 0){
+				$sql  = "INSERT INTO reservation (dtop_id, b_num, slot_id, user_num) ";
+				$sql .= "VALUES ('$desktop', '$build', '$slot[0]', '$curr')";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+			
+				$sql = "CALL set_last_request($curr);";
+				$stmt = $dbh->prepare($sql);
+				$stmt->execute();
+				$row  = "You have successfully reserved " . $desktop . " at " . $time . " on " . $date;
+
+				echo $row;
+				}
+				else{
+					echo "This slot is reserved by someone else or this user has a reservation for a different desktop at this time.";
+				}
+		}
+
+	}	
+else{
+	#echo "You are already in the queue for desktop " . $desktop . " at " . $time . " on " . $date;
+	echo "A request for this timeslot already exists for this user.";
+}
+
 ?>
