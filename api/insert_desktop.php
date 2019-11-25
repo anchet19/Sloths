05d604a406a675f4f7f2038c0bcba7ed0fce0fcd
anchet19 <49288517+anchet19@users.noreply.github.com>
2019-11-25 12:13:41 -0500

    Color selection on new desktop creation

--- api/insert_desktop.php
+++ api/insert_desktop.php
@@ -11,13 +11,14 @@ include_once("./get_user_func.php");
 
 try{
   $dbh = ConnectDB();
-  if(isset($_POST['desktop']) && isset($_POST['username'])){
+  if(isset($_POST['desktop'], $_POST['username'], $_POST['color'])){
     $username = $_POST['username'];
     $name = $_POST['desktop'];
+    $color = $_POST['color'];
     $userData = json_decode(getUser($username));
           
     if($userData->{'admin'} == 2){
-      $sql = "INSERT INTO desktop (name) VALUES('$name')";
+      $sql = "INSERT INTO desktop (name, color) VALUES('$name', '$color')";
       $stmt = $dbh->prepare($sql);
       $stmt->execute();
       
